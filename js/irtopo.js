2d9ddb266e0d4544005e775a3922bbac97adeed7
Leonardo Pinto Silva Calazans <leonardopintosilva@hotmail.com>
2021-01-21 22:31:07 -0300

    button hambuguer menu mobile

--- js/irtopo.js
+++ js/irtopo.js
@@ -1,21 +1,24 @@
-const backTop = document.getElementById('#irtopo');
+document.addEventListener("DOMContentLoaded", function () {
+    const backTop = document.getElementById('#irtopo');
+    document.getElementById(backTop).addEventListener("click", window.top);
+});
 
-$(document).ready(function () {
-    console.log("ready");
-    backTop.style.display = "none";
+// $(document).ready(function () {
+//     console.log("ready");
+//     backTop.style.display = "none";
 
-    $("body").scroll(function () {
-        if ($(this).scrollTop() == 0) {
-            backTop.style.display = "none";
-        } else {
-            backTop.style.display = "block";
-        }
-    });
+//     $("body").scroll(function () {
+//         if ($(this).scrollTop() == 0) {
+//             backTop.style.display = "none";
+//         } else {
+//             backTop.style.display = "block";
+//         }
+//     });
 
-    $(document).ready(function () {
-        $(backTop).click(function () {
-            $('html, body').animate({ scrollTop: 0 }, 800);
-            return false;
-        });
-    })
-})
+//     $(document).ready(function () {
+//         $(backTop).click(function () {
+//             $('html, body').animate({ scrollTop: 0 }, 800);
+//             return false;
+//         });
+//     })
+// })
